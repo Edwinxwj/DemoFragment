4be2971d1f88cda4dec7610ad11c77bf992c13dd
15056215 <15056215@myrp.edu.sg>
2017-06-02 12:36:16 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/demofragment/MainActivity.java
@@ -0,0 +1,38 @@
+package sg.edu.rp.c346.demofragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+
+        //Creating the first fragment (blue)
+        Fragment f1 = new FragmentFirst();
+        ft.replace(R.id.frame1, f1);
+
+        //Creating the second fragment (pink)
+        Fragment f2 = new FragmentSecond();
+        ft.replace(R.id.frame2, f2);
+
+
+        ft.commit();
+
+
+
+
+
+
+    }
+}
